c8636bdaf9327abc3e44c7501e0fc2017d2b8b2e
Marius Herring <mariusherring@gmail.com>
2016-05-27 14:53:48 +0200

    More components

--- app/js/guitar-kit.js
+++ app/js/guitar-kit.js
@@ -2,23 +2,56 @@ var React = require('react');
 var render = require('react-dom').render;
 var Theory = require('./theory');
 
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('example')
-// );
+
+var tmpData = [[{"note": "E"}, {"note": "F"}], [{"note": "A"}], [{"note": "D"}]];
+
+var forEachCreate = function(toProcess, creator) {
+  var arr = [];
+  toProcess.forEach(function(elem) {
+    arr.push(creator(elem));
+  });
+  return arr;
+}
+
+var Fret = React.createClass({
+  render: function() {
+    return (
+      <div className="fret">
+        {this.props.fret.note}
+      </div>
+    );
+  }
+});
+
+var String = React.createClass({
+  render: function() {
+    var frets = forEachCreate(this.props.string, function(fret) {
+      return <Fret fret={fret}/>;
+    });
+    return (
+      <div className="string">
+        {frets}
+      </div>
+    );
+  }
+});
+
 
 
 var Fretboard = React.createClass({
   render: function() {
+    var strings = forEachCreate(this.props.fretboard, function(str) {
+      return <String string={str}/>;
+    });
     return (
       <div className="fretboard">
-        Hello world!
+        {strings}
       </div>
     );
   }
 });
 
 render(
-  <Fretboard />,
+  <Fretboard fretboard={tmpData}/>,
   document.getElementById('example')
 );
